45728edf1ac43d8114c023a06941f57b71bf85d5
Smart-UI web development agency <92440177+Company-Smart-ui@users.noreply.github.com>
2022-05-12 12:52:00 +0300

    header

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,31 @@ import logo from "../images/iproject_logo.svg"
 import logoMobile from "../images/iproject_logo_mobile.svg"
 
 const Header = () => {
+  const [navOpen , setNavOpen ]= React.useState(false);
+  const [getStickyHeader , setStickyHeader] = React.useState(true);
+  React.useEffect(() => {
+          let pscroll = 0
+          const stickyHeader = ()=>{
+            if(typeof window ==undefined){
+                return
+            }
+            const pageYOffset = window.pageYOffset; 
+            if (pscroll>pageYOffset||pageYOffset<100){
+              setStickyHeader(true)          
+              document.body.classList.add('is_sticky')
+            }else { 
+              setStickyHeader(false)   ;
+              document.body.classList.remove('is_sticky') 
+              }  
+              pscroll=pageYOffset;
+          }
+          window.addEventListener('scroll', stickyHeader);
+          return () => {
+            window.removeEventListener('scroll', stickyHeader);
+          }
+        }, [ ])
   return (
-    <header>
+    <header style={{position:getStickyHeader?"fixed":"absolute" }}  className={( getStickyHeader?" header_sticky ":'' ) +( navOpen?"header_open":"")}  >
       <div class="wrapper">
         <div class="header_flex">
           <a href="/" class="header_logo">
@@ -60,12 +83,57 @@ const Header = () => {
             </li>
           </ul>
           <a href="tel:+375293822550" class="header_phone">+375 29 382 25 50</a>
-          <div class="header_burger">
+          <div onClick={()=>  {setNavOpen(!navOpen)}}  class="header_burger">
             <span></span>
             <span></span>
             <span></span>
             <span></span>
           </div>
+        </div>
+        <div class="header_mobile">
+          <p className="header_mobile_nav">Портфолио</p>
+          <ul class="header_mobile_list_1">
+            <li>
+              <Link to="/portfolio/" className="header_mobile_nav_link" activeClassName="header_mobile_nav_active">Все проекты</Link>
+            </li>
+            <li>
+              <Link to="/portfolio/interier/" className="header_mobile_nav_link" activeClassName="header_mobile_active">Интерьеры</Link>
+            </li>
+            <li>
+              <Link to="/portfolio/realize/" className="header_mobile_nav_link" activeClassName="header_mobile_active">Реализованные</Link>
+            </li>
+            <li>
+              <Link to="/portfolio/architecture/" className="header_mobile_nav_link" activeClassName="header_mobile_active">Архитектура</Link>
+            </li>
+          </ul>
+          <div className="header_mobile_line"></div>
+          <p className="header_mobile_nav">Услуги</p>
+          <ul class="header_mobile_list_2">
+            <li>
+              <Link to="/uslugi/dizajn-interera/" className="header_mobile_nav_link" activeClassName="header_mobile_active">Дизайн интерьера квартир</Link>
+            </li>
+            <li>
+              <Link to="/uslugi/dizajn-interera-domov/" className="header_mobile_nav_link" activeClassName="header_mobile_active">Дизайн интерьера домов</Link>
+            </li>
+            <li>
+              <Link to="/uslugi/arxitekturnoe-proektirovanie/" className="header_mobile_nav_link" activeClassName="header_mobile_active">Архитектурное проектирование</Link>
+            </li>
+            <li>
+              <Link to="/uslugi/dizajn-interera-ofisov/" className="header_mobile_nav_link" activeClassName="header_mobile_active">Дизайн офисов</Link>
+            </li>
+            <li>
+              <Link to="/uslugi/stroitelstvo-i-rekonstrukciya/" className="header_mobile_nav_link" activeClassName="header_mobile_active">Строительство и реконструкция</Link>
+            </li>
+            <li>
+              <Link to="/uslugi/avtorskoe-soprovozhdenie/" className="header_mobile_nav_link" activeClassName="header_mobile_active">Авторское сопровождение</Link>
+            </li>
+            <li>
+              <Link to="/uslugi/soglasovanie/" className="header_mobile_nav_link" activeClassName="header_mobile_active">Согласование проекта</Link>
+            </li>
+            <li>
+              <Link to="/uslugi/komplektaciya-obekta/" className="header_mobile_nav_link" activeClassName="header_mobile_active">Комплектация объекта</Link>
+            </li>
+          </ul>
         </div>
       </div>
     </header>
